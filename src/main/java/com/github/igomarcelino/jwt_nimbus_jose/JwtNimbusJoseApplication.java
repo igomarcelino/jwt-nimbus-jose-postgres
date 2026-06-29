9edbe84a15b5a9a45e58aae4d806c22cbbcfdd43
Igo Marcelino <geslucian@hotmail.com>
2026-06-29 18:38:18 -0300

    first commit

---
+++ src/main/java/com/github/igomarcelino/jwt_nimbus_jose/JwtNimbusJoseApplication.java
@@ -0,0 +1,13 @@
+package com.github.igomarcelino.jwt_nimbus_jose;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtNimbusJoseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtNimbusJoseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/igomarcelino/jwt_nimbus_jose/JwtNimbusJoseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.igomarcelino.jwt_nimbus_jose;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtNimbusJoseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
